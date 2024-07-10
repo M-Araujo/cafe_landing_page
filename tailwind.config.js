a6cc8b385be10fd4b2a1b70777bf58d09a62d417
Miriam Araújo <miriamaraujoweb@gmail.com>
2024-07-10 10:20:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        default: '15px',
+      }
+    },
+    screens:{
+      'sm': '640px',
+      'md': '768px',
+      'lg': '960px',
+      'xl': '1330px'
+    },
+    extend: {
+      colors:{
+        primary: '#005696',
+        secondary: '#cccccc',
+        accent: {
+          default: '#29323c',
+          secondary: '#333333',
+          tertiary: '#444444',
+        },
+        grey : '#333333',
+      },
+      fontFamily: {
+        primary: 'Poppins',
+      },
+      boxShadow: {
+        primary: '0px 0px 10px rgba(0, 86, 150, 0.2)',
+        secondary: '0px 0px 10px rgba(0, 86, 150, 0.2)',
+      },
+      backgroundImage: {
+        'services': "url('/images/hero.png')",
+      }
+    },
+  },
+  plugins: [],
+}
