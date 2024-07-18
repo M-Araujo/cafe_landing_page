235f23ea5b165225ee19a9ad36ca65a038a31fcb
Miriam Araújo <miriamaraujoweb@gmail.com>
2024-07-18 10:14:04 +0100

    separating js files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+AOS.init({
+    once: true
+});
+
+window.addEventListener('load', () => {
+    document.documentElement.classList.add('loaded');
+});
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+const mobileLinks = mobileMenu.querySelectorAll('a');
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+    });
+});
